b3903b4a7f59336c6084dd5c6b9f704967f19b41
EC2 Default User <ec2-user@ip-172-31-5-253.us-east-2.compute.internal>
2019-09-19 23:43:57 +0000

    setup api

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const app = express();
 const db = require('./db');
+const { User, Story, Review } = db.models;
 
+app.get('/api/users', async(req, res, next) => {
+    try {
+        res.send(await User.findAll());
+    }
+    catch(ex){
+      next(ex);
+    }
+});
 
 const port = process.env.PORT || 3000;
 db.syncAndSeed()
